6bdeaa56bc904678cd47c1dd152802b3497ee611
winsvts <wins.viswa@gmail.com>
2021-12-23 18:43:34 +0530

    mainn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Blockdetails from './components/Blockdetails.js'
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+            <div className="container">
+
+              <div className="row">
+                  <h1 className="mt-3">
+                    Mother's Village
+                  </h1>
+                  <hr className="mb-3"></hr>
+              </div>
+
+              <div className="row">
+                <div className="col-md-2">
+                  <nav>
+                    <ul className="list-group">
+                    <li className="list-group-item">
+                        <Link to="/">Home</Link>
+                      </li>                
+                     
+                      <li className="list-group-item">
+                        <Link to="/CustomerDetails">Customer Details</Link>
+                      </li>
+                      <li className="list-group-item">
+                        <Link to="/PaymentDetails">Payment Details</Link>
+                      </li>
+                      <li className="list-group-item">
+                        <Link to="/Blockdetails">Block details</Link>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+
+                <div className="col-md-10">
+                  <Switch>
+                  <Route path="/CustomerDetails">
+                      <CustomerDetails />
+                    </Route>
+                    <Route path="/PaymentDetails">
+                      <PaymentDetails />
+                    </Route>
+                    <Route path="/Blockdetails">
+                      <Blockdetails />
+                    </Route>
+                    <Route path="/">
+                      <Home />
+                    </Route>
+                  </Switch>
+                </div>
+              </div>
+            </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function CustomerDetails(){
+  return <h2>Customers Details</h2>
+}
+function PaymentDetails() {
+  return <h2>Payment Details</h2>
+}
+
