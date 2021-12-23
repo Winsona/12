e1295c5702c70eb31e515d536a43c39e43d44bb9
winsvts <wins.viswa@gmail.com>
2021-12-23 18:49:16 +0530

    Update Blockdetails.js

--- src/components/Blockdetails.js
+++ src/components/Blockdetails.js
@@ -5,7 +5,7 @@ export default class Blockdetails extends Component {
     render(){
         return (
             <h2>Block:</h2>
-            /*<h2>Floor:</h2>
+           /* <h2>Floor:</h2>
             <h2>Flat No:</h4>
             <h2>Name:</h2>*/
 
